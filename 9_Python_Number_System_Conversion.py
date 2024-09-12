71784a54e3b22cb19177985e3fa3163d296989d7
Jakub Kujawski <jaahcob@gmail.com>
2024-09-13 00:07:51 +0100

    Add files via upload

---
+++ 9_Python_Number_System_Conversion.py
@@ -0,0 +1,44 @@
+Python 3.12.5 (v3.12.5:ff3bc82f7c9, Aug  7 2024, 05:32:06) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #Number System Conversion
+>>> #Binary, Decimal, Octal, HexaDecimal Systems
+>>> 
+>>> #MAC address (Media Access Control address) use HexaDecimal 12digit number assigned to each device connected to the network
+>>> 
+>>> #IPv6 (Internet Protocol version 6) - provides the unique IP addresses necessary for Internet-enabled devices to communicate also use HexaDecimal System
+>>> 
+>>> #To Convert Decimal System to Binary I use function bin()
+>>> 
+>>> bin(25)
+'0b11001'
+>>> 
+>>> # In Decimal system numbers are from 0 - 9 (10 digits)
+>>> # In Binary system numbers are from 0 - 1 ( 2 digits)
+>>> # In Octal system numbers are from 0 - 7 (8 digits)
+>>> # In HexaDecimal system numbers are from 0 - 9 a - f (16 digits)
+>>> 
+>>> bin(25)
+'0b11001'
+>>> 
+>>> # 0b means its a Binary format and the rest it's the number 25 = 11001
+>>> 
+>>> oct(25)
+'0o31'
+>>> 
+>>> # 0o means it's a Octal format and the rest is the number 25 = 31
+>>> 
+>>> hex(25)
+'0x19'
+>>> 
+>>> # 0x means it's a HexaDecimal format and the rest is the number 25 = 19
+>>> 
+>>> hex(10)
+'0xa'
+>>> 
+0xf
+15
+
+#This is important for Bitwise Operators (used to perform bitwise calculations on integers)
+
+0b110011010
+410
